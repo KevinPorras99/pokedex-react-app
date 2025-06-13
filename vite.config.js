fa9171bc5c2c6d10da77b06d0ec0726201a28b8c
Kevin Porras <kevinporras9906@gmail.com>
2025-06-13 17:54:16 -0600

    Cambios para produccion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Add this line - use your repo name
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
