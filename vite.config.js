b65f1438fa6013959f4c89749efdaa174847393e
Kevin Porras <kevinporras9906@gmail.com>
2025-06-13 17:24:23 -0600

    Cambio en el vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/pokedex-react-app/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/pokedex-react-app/' : '/', // 👈 clave aquí
   plugins: [react()],
-})
+}))
